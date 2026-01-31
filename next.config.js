598a62156ff5138e86e06c59a25df2d586d99a5c
ljy <ljy06027@naver.com>
2026-02-01 06:30:50 +0900

    Remove embedded caltest git repository

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/caltest',
+  distDir: 'out'
+}
+
+module.exports = nextConfig
---
+++ out/_next/static/chunks/cef32923611bf206.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/dbdcc9015c285211.js",
+  "static/chunks/222f710509a5edc6.js",
+  "static/chunks/turbopack-b22893768b127a5c.js"
+])
