3de3cf62911f94f148c71dc0287b5cfe47a9d20b
ljy <ljy06027@naver.com>
2026-02-01 06:55:58 +0900

    Updates

--- _next/static/chunks/f2379151c6c8aaf1.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/00a7e2169ac3d3d2.js",
-  "static/chunks/222f710509a5edc6.js",
-  "static/chunks/turbopack-8803209a4226aee0.js"
-])
--- _next/static/chunks/f941819cf1d5289e.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/a3ef7e6334867ded.js",
-  "static/chunks/222f710509a5edc6.js",
-  "static/chunks/turbopack-e92043c23eb40065.js"
-])
